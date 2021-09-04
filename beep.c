3df245f6ffebf788f3947f06167cd6c3a9eeaaa1
Noah Johnson <noah.johnson@loupmail.com>
2021-09-04 11:30:20 -0500

    Update beep.c

--- beep.c
+++ beep.c
@@ -3,25 +3,28 @@
  */
 
 #include "wavwrite.h"
+#include "sqwave.h"
 #include <math.h>
+#include <stdint.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   
-  long scount = 0;
-  double f = 0.0;
+  int32_t scount = 0;
   int v = 0;
   
+  /* Initialize square wave module */
+  sqwave_init(1024, 0.3, 20000);
+  
   /* Initialize WAV writer */
-  if (!wavwrite_init("a440.wav",
+  if (!wavwrite_init("a440_filter.wav",
                       WAVWRITE_INIT_44100 | WAVWRITE_INIT_MONO)) {
     abort();
   }
   
-  /* Write five seconds of 440 Hz tone */
+  /* Write five seconds of 440 Hz square wave */
   for(scount = 0; scount < (44100 * 5); scount++) {
-    f = sin(2 * M_PI * ((double) scount) * (440.0 / 44100.0));
-    v = (int) (f * 20000.0);
+    v = (int) sqwave_get(440, 44100, (int32_t) scount);
     wavwrite_sample(v, v);
   }
   
