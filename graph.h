2b04f4282e8018130af44134ef884f541d168ff9
Noah Johnson <noah.johnson@loupmail.com>
2021-09-04 13:16:13 -0500

    Update graph.h

--- graph.h
+++ graph.h
@@ -13,8 +13,16 @@
 /*
  * The maximum intensity value in a graph.
  */
-#define GRAPH_MAXVAL (UINT16_C(32768))
+#define GRAPH_MAXVAL (UINT16_C(1024))
 
 /* @@TODO: */
+struct GRAPH_OBJ_TAG;
+typedef struct GRAPH_OBJ_TAG GRAPH_OBJ;
+
+GRAPH_OBJ *graph_alloc(int32_t count);
+void graph_addref(GRAPH_OBJ *pg);
+void graph_release(GRAPH_OBJ *pg);
+void graph_set(
+  GRAPH_OBJ *pg, int32_t i, int32_t t, int32_t ra, int32_t rb);
 
 #endif
