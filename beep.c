199fcbbe3345dfa2e628b2f4aaa5f005fef65a8b
Noah Johnson <noah.johnson@loupmail.com>
2021-09-04 11:37:56 -0500

    Update beep.c

--- beep.c
+++ beep.c
@@ -86,13 +86,72 @@ static void sinewave(
   }
 }
 
+/*
+ * Add a square wave to the given sample array.
+ * 
+ * This is a wrapper around sinewave.  See that function for a
+ * description of the ps, samp_count, wave_count, and amp parameters.
+ * 
+ * The square wave is constructed as multiple calls to sinewave, each
+ * adding a different harmonic of the square wave.  Square waves only
+ * include odd-numbered harmonics.
+ * 
+ * harmonics indicates how many harmonics to add.  It must be greater
+ * than zero.  If it is one, the result is the same as a sine wave.
+ * Higher numbers get closer to a square wave.
+ * 
+ * Parameters:
+ * 
+ *   ps - pointer to the sample array
+ * 
+ *   samp_count - the number of samples in the sample array
+ * 
+ *   wave_count - the number of wave periods to write
+ * 
+ *   amp - the amplitude of the sine wave
+ * 
+ *   harmonics - the number of harmonics to add
+ */
+static void squarewave(
+    int16_t * ps,
+    int32_t   samp_count,
+    int32_t   wave_count,
+    double    amp,
+    int32_t   harmonics) {
+  
+  int32_t h = 0;
+  int32_t m = 0;
+  
+  /* Check harmonics, wave_count, and amp parameters */
+  if ((harmonics < 1) || (wave_count < 1)) {
+    abort();
+  }
+  if (!isfinite(amp)) {
+    abort();
+  }
+  
+  /* Add harmonics */
+  for(h = 0; h < harmonics; h++) {
+    
+    /* Get multiplier */
+    m = (h * 2) + 1;
+    
+    /* Call through */
+    sinewave(
+        ps,
+        samp_count,
+        wave_count * m,
+        (4.0 / (M_PI * ((double) m))) * amp);
+  }
+}
+
 int main(int argc, char *argv[]) {
   
   int32_t scount = 0;
   int16_t *ps = NULL;
   
   /* Initialize WAV writer */
-  if (!wavwrite_init("a440_sine.wav",
+  if (!wavwrite_init("a440_square.wav",
                       WAVWRITE_INIT_44100 | WAVWRITE_INIT_MONO)) {
     abort();
   }
@@ -104,8 +163,8 @@ int main(int argc, char *argv[]) {
   }
   memset(ps, 0, 44100 * 5 * sizeof(int16_t));
   
-  /* Write a sine wave */
-  sinewave(ps, 44100 * 5, 440 * 5, 20000.0);
+  /* Write a square wave */
+  squarewave(ps, 44100 * 5, 440 * 5, 20000.0, 8);
   
   /* Write buffer to output */
   for(scount = 0; scount < (44100 * 5); scount++) {
