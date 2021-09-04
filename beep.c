ea72ac33718b04cc3d91e5f9a25e89a33eed875f
Noah Johnson <noah.johnson@loupmail.com>
2021-09-04 11:27:53 -0500

    Update beep.c

--- beep.c
+++ beep.c
@@ -3,21 +3,26 @@
  */
 
 #include "wavwrite.h"
+#include <math.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   
   long scount = 0;
+  double f = 0.0;
+  int v = 0;
   
   /* Initialize WAV writer */
-  if (!wavwrite_init("silence.wav",
+  if (!wavwrite_init("a440.wav",
                       WAVWRITE_INIT_44100 | WAVWRITE_INIT_MONO)) {
     abort();
   }
   
-  /* Write five seconds of silence */
+  /* Write five seconds of 440 Hz tone */
   for(scount = 0; scount < (44100 * 5); scount++) {
-    wavwrite_sample(0, 0);
+    f = sin(2 * M_PI * ((double) scount) * (440.0 / 44100.0));
+    v = (int) (f * 20000.0);
+    wavwrite_sample(v, v);
   }
   
   /* Close down */
