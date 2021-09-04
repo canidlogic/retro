3d0723a7e53034198bd4eef9c08e3b1f77abe675
Noah Johnson <noah.johnson@loupmail.com>
2021-09-04 12:49:24 -0500

    Update adsr.h

--- adsr.h
+++ adsr.h
@@ -21,87 +21,23 @@
 #define ADSR_MAXTIME (INT32_C(100000000))
 
 /*
- * The ADSR parameters structure.
- * 
- * Clients should not access the internal fields directly.  Use the
- * functions provided by this module instead.
+ * The maximum integer intensity value in an envelope.
  */
-typedef struct {
-  
-  /*
-   * The maximum intensity multiplier for the instrument.
-   * 
-   * This is a finite value in range (0.0, 1.0].  It must be greater
-   * than or equal to i_min.  If i_max and i_min are the same, then the
-   * instrument will have constant intensity regardless of the input
-   * intensity.
-   */
-  double i_max;
-  
-  /*
-   * The minimum intensity multiplier for the instrument.
-   * 
-   * This is a finite value in range [0.0, 1.0].  It must be less than
-   * or equal to i_max.  If i_max and i_min are the same, then the
-   * instrument will have constant intensity regardless of the input
-   * intensity.
-   */
-  double i_min;
-  
-  /*
-   * The sustain multiplier for the instrument.
-   * 
-   * This is a finite value in range [0.0, 1.0].
-   * 
-   * If the sustain is 1.0, then the decay must be zero, since there is
-   * no difference between the attack intensity and the sustain
-   * intensity in this case.
-   * 
-   * If the sustain is 0.0, then the release must be zero, since the
-   * sound has already finished by the time the release begins.
-   */
-  double sustain;
-  
-  /*
-   * The attack duration, in samples.
-   * 
-   * This must be in range [0, ADSR_MAXTIME].
-   */
-  int32_t attack;
-  
-  /*
-   * The decay duration, in samples.
-   * 
-   * This must be in range [0, ADSR_MAXTIME].
-   * 
-   * If the sustain value is 1.0, then the decay duration must be zero.
-   */
-  int32_t decay;
-  
-  /*
-   * The release duration, in samples.
-   * 
-   * This must be in range [0, ADSR_MAXTIME].
-   * 
-   * If the sustain value is 0.0, then the release duration must be
-   * zero.
-   */
-  int32_t release;
-  
-} ADSR_PARAM;
+#define ADSR_MAXVAL (16384)
 
 /*
- * Initialize an ADSR parameter structure.
+ * Structure prototype for ADSR_OBJ.
+ */
+struct ADSR_OBJ_TAG;
+typedef struct ADSR_OBJ_TAG ADSR_OBJ;
+
+/*
+ * Create an ADSR envelope object.
  * 
- * This must be called before an ADSR_PARAM structure can be used with
- * the other functions of this module.  The structure need not be
- * cleaned up in any way before being released, and it can be re-used
- * by simply re-initializing it.
- * 
- * pa points to the structure to initialize.
+ * The returned object must eventually be freed with adsr_free().
  * 
  * i_max and i_min are the maximum and minimum intensity multipliers for
- * the instrument.  Both must be finite values in range [0.0, 1.0].
+ * the envelope.  Both must be finite values in range [0.0, 1.0].
  * Additionally, i_max must be greater than or equal to i_min, and i_max
  * may not be 0.0.
  * 
@@ -118,6 +54,10 @@ typedef struct {
  * transformed into a sample count this value would exceed ADSR_MAXTIME,
  * the duration is shortened to ADSR_MAXTIME.
  * 
+ * The envelope (excluding the sustain period) is cached in memory with
+ * two bytes per sample, so be cautious of specifying long attack,
+ * decay, or release times.
+ * 
  * sustain is the sustain level multiplier.  It must be finite and in
  * range [0.0, 1.0].  If it is 1.0, then t_decay is ignored and the
  * decay is set to zero.  If it is 0.0, then t_release is ignored and
@@ -126,8 +66,6 @@ typedef struct {
  * rate is the sampling rate, in Hz.  It must be either 44100 or 48000.
  * 
  * Parameters:
- * 
- *   pa - pointer to the structure to initialize
  * 
  *   i_max - the maximum intensity multiplier
  * 
@@ -143,8 +81,7 @@ typedef struct {
  * 
  *   rate - the sampling rate, in Hz
  */
-void adsr_init(
-    ADSR_PARAM * pa,
+ADSR_OBJ *adsr_alloc(
     double       i_max,
     double       i_min,
     double       t_attack,
@@ -154,52 +91,67 @@ void adsr_init(
     int32_t      rate);
 
 /*
- * Get the number of extra samples that should be added to each note
- * duration to account for the release time.
+ * Free a given ADSR envelope object.
  * 
- * The return value is in range [0, ADSR_MAXTIME].
+ * pa points to the object to free.  If it is NULL, the call is ignored.
  * 
  * Parameters:
  * 
- *   pa - pointer to the initialized ADSR structure
+ *   pa - the ADSR envelope object to free, or NULL
+ */
+void adsr_free(ADSR_OBJ *pa);
+
+/*
+ * Given an event duration in samples, get the ADSR envelope length in
+ * samples.
+ * 
+ * The given duration must be greater than zero.  The return value will
+ * also be greater than zero.
+ * 
+ * The return value might be less than, equal to, or greater than dur,
+ * depending on the particular envelope.
+ * 
+ * Parameters:
+ * 
+ *   pa - the ADSR object
+ * 
+ *   dur - the duration of the event in samples
  * 
  * Return:
  * 
- *   the number of extra samples
+ *   the duration of the envelope in samples for the event
  */
-int32_t adsr_extra(const ADSR_PARAM *pa);
+int32_t adsr_length(ADSR_OBJ *pa, int32_t dur);
 
 /*
  * Transform a given sample according to an ADSR envelope.
  * 
- * pa points to the initialized ADSR structure.
- * 
  * t is the time offset in samples from the beginning of the envelope.
  * It must be zero or greater.
  * 
- * dur is the duration of the event in samples, not including the extra
- * samples for the release.  It must be greater than zero.
+ * dur is the duration of the event in samples.  This is not necessarily
+ * the same as the duration of the envelope.  It must be greater than
+ * zero.
  * 
- * The ADSR envelope is defined for t in [0, (dur + extra - 1)], where
- * extra is the return value of adsr_extra() for this envelope.  Any t
- * value greater than this range will result in a return value of zero.
+ * The ADSR envelope is defined for t in [0, (length - 1)], where length
+ * is the return value of adsr_length() for dur.  Any t value greater
+ * than this range will result in a sample value of zero.
  * 
  * intensity is the intensity of the event at time offset t.  It must be
- * in range [0.0, 1.0], where 0.0 maps to the minimum intensity
- * multiplier for this instrument and 1.0 maps to the maximum intensity
- * multiplier for this instrument.
+ * in range [0, ADSR_MAXVAL], where zero maps to the minimum intensity
+ * multiplier for this envelope and ADSR_MAXVAL maps to the maximum
+ * intensity multiplier for this envelope.
  * 
  * s is the input sample to transform by the ADSR envelope.  The return
  * value is the transformed sample.
  * 
  * Parameters:
  * 
- *   pa - the initialized ADSR structure
+ *   pa - the ADSR object
  * 
  *   t - the time offset in samples from the start of the envelope
  * 
- *   dur - the duration of the event in samples, not including the extra
- *   time for release
+ *   dur - the duration of the event in samples
  * 
  *   intensity - the intensity at time t
  * 
@@ -210,10 +162,10 @@ int32_t adsr_extra(const ADSR_PARAM *pa);
  *   the transformed sample
  */
 int16_t adsr_mul(
-    const ADSR_PARAM * pa,
-          int32_t      t,
-          int32_t      dur,
-          double       intensity,
-          int16_t      s);
+    ADSR_OBJ * pa,
+    int32_t    t,
+    int32_t    dur,
+    int32_t    intensity,
+    int16_t    s);
 
 #endif
