4e565937e9cc4829fdf1f23fa8aa7212c75aefc6
Noah Johnson <noah.johnson@loupmail.com>
2023-01-05 20:17:48 -0600

    Register write ordering rules

--- Retro/Assembler.pm
+++ Retro/Assembler.pm
@@ -13,6 +13,8 @@ Retro::Assembler - OPL2 hardware script assembler module.
 
 =head1 SYNOPSIS
 
+  use Retro::Assembler;
+  
   # Construct a new assembler instance with 60 Hz control rate
   my $asm = Retro::Assembler->create(60);
   
@@ -43,6 +45,105 @@ When all the information has been specified, call C<build()> to create
 the completed hardware script.  All the register writes that were
 scheduled will be sorted in chronological order, and the hardware script
 will proceed through time sequentially.
+
+Within each time point, the order of the hardware register writes
+follows a few rules to keep the state of the OPL2 consistent.  See the
+documentation of C<build()> for further information.
+
+=cut
+
+# ===============
+# Local functions
+# ===============
+
+# _order_writes($fh, \%reg_map)
+# -----------------------------
+#
+# Output a sequence of "r" instructions at the same time point, with the
+# registers in the proper order within the time point.
+#
+# The hash map has keys that are two lowercase base-16 digits.  The
+# values are integers in range 0-255.
+#
+
+# Maps all the keys of registers having special ordering rules to value
+# of one.
+#
+my %ORDERED_REGISTERS = (
+  '01' => 1,
+  '08' => 1,
+  'b0' => 1, 'b1' => 1, 'b2' => 1, 'b3' => 1, 'b4' => 1,
+  'b5' => 1, 'b6' => 1, 'b7' => 1, 'b8' => 1,
+  'bd' => 1
+);
+
+sub _order_writes {
+  # Get parameters
+  ($#_ == 1) or die;
+  
+  my $fh = shift;
+  
+  my $reg_map = shift;
+  (ref($reg_map) eq 'HASH') or die;
+  
+  # Write registers 01 and 08 first if they are within the hash
+  if (defined $reg_map->{'01'}) {
+    printf { $fh } "r 01 %02x\n", $reg_map->{'01'};
+  }
+  if (defined $reg_map->{'08'}) {
+    printf { $fh } "r 08 %02x\n", $reg_map->{'08'};
+  }
+  
+  # Write register BD here only if its rhythm mode flag is OFF
+  if (defined $reg_map->{'bd'}) {
+    if (($reg_map->{'bd'} & 0x20) == 0) {
+      printf { $fh } "r bd %02x\n", $reg_map->{'bd'};
+    }
+  }
+  
+  # For the registers B0-B8, write them here only if their key-down flag
+  # is OFF
+  for(my $reg = 0xb0; $reg <= 0xb8; $reg++) {
+    my $addr = sprintf("%02x", $reg);
+    if (defined $reg_map->{$addr}) {
+      if (($reg_map->{$addr} & 0x20) == 0) {
+        printf { $fh } "r %s %02x\n", $addr, $reg_map->{$addr};
+      }
+    }
+  }
+  
+  # Write all registers except 01, 08, B0-B8, and BD here
+  for my $k (keys $reg_map) {
+    # Skip keys with special ordering rules
+    if (defined $ORDERED_REGISTERS{$k}) {
+      next;
+    }
+    
+    # Make sure key has proper format
+    ($k =~ /^[0-9a-f]{2}$/) or die;
+    
+    # Write the register
+    printf { $fh } "r %s %02x\n", $k, $reg_map->{$k};
+  }
+  
+  # For the registers B0-B8, write them here only if their key-down flag
+  # is ON
+  for(my $reg = 0xb0; $reg <= 0xb8; $reg++) {
+    my $addr = sprintf("%02x", $reg);
+    if (defined $reg_map->{$addr}) {
+      if (($reg_map->{$addr} & 0x20) != 0) {
+        printf { $fh } "r %s %02x\n", $addr, $reg_map->{$addr};
+      }
+    }
+  }
+  
+  # Write register BD here only if its rhythm mode flag is ON
+  if (defined $reg_map->{'bd'}) {
+    if (($reg_map->{'bd'} & 0x20) != 0) {
+      printf { $fh } "r bd %02x\n", $reg_map->{'bd'};
+    }
+  }
+}
 
 =head1 CONSTRUCTOR
 
@@ -236,6 +337,18 @@ the same time, provided that all the writes have the same value.  If any
 of the writes have a different value, however, an error occurs.  In the
 output hardware script, redundant writes will be dropped.
 
+To ensure a consistent OPL2 state within each time point, hardware
+writes scheduled at the same point will be ordered according to the
+following system:
+
+  1. Register 01
+  2. Register 08
+  3. Register BD if rhythm mode flag is OFF
+  4. Registers B0-B8 which have their key-down flag OFF
+  5. All registers except 01, 08, B0-B8, and BD
+  6. Registers B0-B8 which have their key-down flag ON
+  7. Register BD if rhythm mode flag is ON
+
 =cut
 
 sub build {
@@ -274,11 +387,10 @@ sub build {
     # The t value starts out at zero
     my $t = 0;
     
-    # The most recent record starts out undef; after the first record,
-    # this will be an array of three values, with the first being the
-    # time offset, the second being the register address, and the third
-    # being the data value that was most recently output
-    my $recent = undef;
+    # The reg_map dictionary maps lowercase two-digit base-16 addresses
+    # to scalar values 0-255 within a time point, or undef if not
+    # defined yet
+    my $reg_map = undef;
     
     # Process all the sorted register write instructions
     for my $rec (@wlist) {
@@ -289,39 +401,43 @@ sub build {
       my $addr = $lo >>   8;
       my $val  = $lo & 0xff;
       
-      # If the recent record is defined and both its time offset and
-      # register address are the same as the current record, then we
-      # need to skip this record if its value is the same as the
-      # previous record or raise an error if its value is different
-      if ((defined $recent) and
-          ($recent->[0] == $offs) and ($recent->[1] == $addr)) {
-        if ($val == $recent->[2]) {
-          next;
-        } else {
-          die "Conflicting writes scheduled in assembler!\n";
-        }
-      }
-      
-      # Update the recent array to match the current record, defining it
-      # if necessary
-      if (defined $recent) {
-        $recent->[0] = $offs;
-        $recent->[1] = $addr;
-        $recent->[2] = $val ;
-      } else {
-        $recent = [$offs, $addr, $val];
-      }
-      
       # If our current t value is less than the offset of this record,
-      # then insert a wait instruction bringing the t value up to the
-      # current record's offset
+      # then output current register map if defined and insert a wait
+      # instruction bringing the t value up to the current record's
+      # offset
       if ($t < $offs) {
+        if (defined $reg_map) {
+          _order_writes($fh, $reg_map);
+          $reg_map = undef;
+        }
         printf { $fh } "w %u\n", ($offs - $t);
         $t = $offs;
       }
       
-      # Perform the scheduled write
-      printf { $fh } "r %02x %02x\n", $addr, $val;
+      # If register map not defined, define a new one
+      unless (defined $reg_map) {
+        $reg_map = {};
+      }
+      
+      # Convert address to string format
+      $addr = sprintf("%02x", $addr);
+      
+      # Check whether address already in register map
+      if (defined $reg_map->{$addr}) {
+        # Register write already defined; make sure value is the same
+        ($reg_map->{$addr} == $val) or
+          die "Inconsistent hardware writes in assembler!\n";
+        
+      } else {
+        # Register write not already defined, so define it
+        $reg_map->{$addr} = $val;
+      }
+    }
+    
+    # If register map is defined, write it
+    if (defined $reg_map) {
+      _order_writes($fh, $reg_map);
+      $reg_map = undef;
     }
     
     # If the t value is less than the total duration, add a wait
